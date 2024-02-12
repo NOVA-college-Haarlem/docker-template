fcc0976e9c70e4f2b9a3622e32136859e83fd3f8
David van Oudheusden <doudheusden@novacollege.nl>
2024-02-12 15:12:52 +0100

    new config

---
+++ .docker/phpmyadmin/config.user.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+$cfg['Servers'][1]['auth_type'] = 'config';
+$cfg['Servers'][1]['host'] = 'mariadb'; //edit if you have db in the other host
+$cfg['Servers'][1]['connect_type'] = 'tcp';
+$cfg['Servers'][1]['compress'] = false;
+$cfg['Servers'][1]['extension'] = 'mysql';
+$cfg['Servers'][1]['user'] = 'root'; //edit this line
+$cfg['Servers'][1]['password'] = 'password'; // edit this line 
